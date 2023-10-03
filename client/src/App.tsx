1dd98fbfa7b749331c92582963fe71ed578330fe
squach28 <squach3443@gmail.com>
2023-10-03 09:21:48 -0700

    changed card so that artist is in background of card

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,52 @@
+import { useEffect, useState } from "react";
 import ArtistCard from "./components/ArtistCard"
 
+
+export type ArtistImage = {
+  width: number;
+  height: number;
+  url: string;
+
+}
+
+export type Artist = {
+  id: string;
+  followers: {
+    href: string | null;
+    total: number;
+  },
+  images: ArtistImage[],
+  name: string;
+  
+}
+
 const App = () => {
+  const [artists, setArtists] = useState<Artist[]>([])
+  const [streak, setStreak] = useState<number>(0)
+  const [index, setIndex] = useState<number>(0)
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/artist/getArtistsByCategory?category=pop`, {
+      credentials: 'include'
+    })
+      .then(res => res.json())
+      .then(data => setArtists(data))
+  }, [])
 
   return (
-    <div className="w-full bg-black text-white flex flex-col p-4 space-around">
-      <p>Which artist is more popular?</p>
-      <ArtistCard />
+    artists.length > 0 ?
+    <div className="w-full h-screen bg-black text-white flex flex-col p-4 space-around items-center">
+      <div className="w-full flex justify-between items-center">
+        <h1 className="text-3xl font-bold">Follower Face Off</h1>
+        <p>Streak: {streak}</p>
+      </div>
+      <p>Which artist has more followers?</p>
+      <ArtistCard {...artists[index]}/>
       <p className="text-center text-2xl">OR</p>
-      <ArtistCard />
+      <ArtistCard {...artists[index + 1]} />
     </div>
+    :
+    <div>Loading...</div>
 
   )
 }
--- client/src/components/ArtistCard.tsx
+++ client/src/components/ArtistCard.tsx
@@ -1,15 +1,12 @@
-const ArtistCard = () => {
-    return (
-      <div className="w-full bg-white text-black flex  flex-col items-center gap-3 p-6 rounded-md">
-        <img width={120} className="rounded-full" src="https://i.scdn.co/image/ab6761610000f1785da361915b1fa48895d4f23f" alt="newjeans!" />
-        <p className="text-xl">NewJeans</p>
-        <p>Top Tracks</p>
-        <ul className="flex flex-col justify-center items-center">
-          <li>1. Super Shy</li>
-          <li>2. ETA</li>
-          <li>3. OMG</li>
-        </ul>
-      </div>
+import { Artist } from "../App"
+
+const ArtistCard = (artist: Artist) => {
+
+  console.log(artist.images[0])
+  return (
+    <div style={{backgroundImage: `url('${artist.images[0].url}')`}} className={`w-[320px] h-[320px] bg-no-repeat bg-cover text-white flex flex-col justify-center items-center gap-3 p-6 rounded-md`}>
+      <p className="text-4xl font-bold">{artist.name}</p>
+    </div>
     )
 }
 

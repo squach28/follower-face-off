1f70d50e737ccd1570a5163b6ef59025afd3dd65
squach28 <squach3443@gmail.com>
2023-11-03 12:19:15 -0700

    change api url based on environment

--- client/src/components/Dropdown.tsx
+++ client/src/components/Dropdown.tsx
@@ -19,15 +19,13 @@ const Dropdown: React.FC<DropdownProps> = (dropdownProps: DropdownProps) => {
         }
         setShowOptions(false)
     }
-
-    console.log(categoryContext)
     return (
         <div className="bg-white px-2 py-1 select-none relative text-center">
-            <p onClick={toggleShowOptions} className="min-w-[100px] bg-white text-black">{categoryContext?.category}</p>
+            <p onClick={toggleShowOptions} className="min-w-[100px] bg-white text-black hover:cursor-pointer">{categoryContext?.category}</p>
                 {
                     showOptions ?
                         <ul className={`${showOptions ? 'absolute' : 'hidden'} bg-white top-[100%] right-0 w-full`} >
-                            {dropdownProps.options.map(dropdown => <li className={`w-full ${categoryContext!.category === dropdown ? 'bg-black text-white font-bold' : ''}`} key={dropdown} onClick={() => setOption(dropdown)}>{dropdown}</li>)}
+                            {dropdownProps.options.map(dropdown => <li className={`w-full ${categoryContext!.category === dropdown ? 'bg-black hover:bg-black text-white font-bold' : ''} hover:cursor-pointer hover:bg-[#23395B]`} key={dropdown} onClick={() => setOption(dropdown)}>{dropdown}</li>)}
                         </ul>
                         :
                         null
